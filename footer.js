8bde6e11589a4f7579e55b846cecfc472ceb34be
Bradley Hobbs <brad.w.hobbs@gmail.com>
2026-06-18 13:15:51 -0400

    Add footer loading functionality with fetch

---
+++ footer.js
@@ -0,0 +1,25 @@
+fetch("footer.html")
+
+.then(response => response.text())
+
+.then(data => {
+
+document.getElementById(
+
+"footer-container"
+
+).innerHTML = data;
+
+})
+
+.catch(error =>
+
+console.error(
+
+"Footer failed to load.",
+
+error
+
+)
+
+);
